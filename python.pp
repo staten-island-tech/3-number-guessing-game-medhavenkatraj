bfe5884beffdf3d697ed14d7e41a617ff59ee062
medhavenkatraj <medhav@nycstudents.net>
2025-10-17 07:14:51 -0700

    10/17

---
+++ python.pp
@@ -0,0 +1,19 @@
+""" count = 1
+while count <= 5:
+    print("This is loop number", count)
+    count = count + 1
+    if 
+import random
+if random !< 5: """
+
+number_game = function
+number_game = int(input("guess a number. "))
+import random
+x = random.radint(1,10)
+while x != int(input):
+    print("incorrect")
+while x == int(input):
+    print("correct")
+    break
+print("thanks for playing")
+
